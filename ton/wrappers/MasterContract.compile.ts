6decb5c3548c1728484e929198e99918bcedb495
Egor Zhelagin <zhelagin.egor@outlook.com>
2024-05-25 23:16:29 +0300

    Протестировал контракты

--- ton/wrappers/MasterContract.compile.ts
+++ ton/wrappers/MasterContract.compile.ts
@@ -3,4 +3,7 @@ import { CompilerConfig } from '@ton/blueprint';
 export const compile: CompilerConfig = {
     lang: 'tact',
     target: 'contracts/master_contract.tact',
+    options: {
+        debug: true
+    }
 };
